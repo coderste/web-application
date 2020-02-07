76f0ee8974bca913260aa16591281317aade48a8
Stephen Hinett <stephen.hinett@skybettingandgaming.com>
2020-02-07 10:11:57 +0000

    added: templating for storing reuseable html
    Also add a fileServer to serve our static files from

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -12,6 +12,13 @@ func main() {
 	mux.HandleFunc("/snippet", showSnippet)
 	mux.HandleFunc("/snippet/create", createSnippet)
 
+	// Create a file server which serves files out of the "./ui/static" directory.
+	fileServer := http.FileServer(http.Dir("./ui/static"))
+
+	// Use the mux.Handle() function register the file server as the handler
+	// for all URL paths that start with "/static".
+	mux.HandleFunc("/static/", http.StripPrefix("/static", fileServer))
+
 	log.Println("Starting server on :4000")
 	err := http.ListenAndServe(":4000", mux)
 	log.Fatal(err)
