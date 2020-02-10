fa6e3871715154e9f35aefcb05fc1825412fd65e
Stephen Hinett <stephen.hinett@skybettingandgaming.com>
2020-02-10 10:35:31 +0000

    modified: updated the handlers file so the functions are methods on the application struct to access the errorLog and infoLog

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -4,18 +4,35 @@ import (
 	"flag"
 	"log"
 	"net/http"
+	"os"
 	"strings"
 )
+
+type application struct {
+	errorLog *log.Logger
+	infoLog *log.Logger
+}
 
 func main() {
 	addr := flag.String("addr", ":4000", "HTTP Network Address")
 	flag.Parse()
 
+	// Using log.New() to create a logger for writing information messages
+	infoLog := log.New(os.Stdout, "INFO:\t", log.Ldate|log.Ltime)
+
+	// Create a logger for writing error messages in the same way
+	errorLog := log.New(os.Stdout, "ERROR:\t", log.Ldate|log.Ltime|log.Lshortfile)
+
+	app := &application{
+		errorLog: errorLog,
+		infoLog: infoLog,
+	}
+
 	mux := http.NewServeMux()
 
-	mux.HandleFunc("/", home)
-	mux.HandleFunc("/snippet/", showSnippet)
-	mux.HandleFunc("/snippet/create", createSnippet)
+	mux.HandleFunc("/", app.home)
+	mux.HandleFunc("/snippet/", app.showSnippet)
+	mux.HandleFunc("/snippet/create", app.createSnippet)
 
 	// Create a file server which serves files out of the "./ui/static" directory.
 	fileServer := http.FileServer(neuteredFileSystem{http.Dir("./ui/static")})
@@ -25,9 +42,18 @@ func main() {
 	mux.Handle("/static", http.NotFoundHandler())
 	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
 
-	log.Printf("Starting server on %s", *addr)
-	err := http.ListenAndServe(*addr, mux)
-	log.Fatal(err)
+	// Initialize a new http.Server struct. We set the Addr and Handler fields so
+	// that the server uses the same network address and routes as before, and set the ErrorLog
+	// field so that the server now uses the custom errorLog logger
+	server := &http.Server{
+		Addr: *addr,
+		ErrorLog: errorLog,
+		Handler: mux,
+	}
+
+	infoLog.Printf("Starting server on %s", *addr)
+	err := server.ListenAndServe()
+	errorLog.Fatal(err)
 }
 
 type neuteredFileSystem struct {
