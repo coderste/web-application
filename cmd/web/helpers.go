497041176248d3344c821d566785b46e6d42ab94
Stephen Hinett <stephen.hinett@skybettingandgaming.com>
2020-02-10 16:50:11 +0000

    modified: now we have centralized the error handling inside a helpers.go file

---
+++ cmd/web/helpers.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"runtime/debug"
+)
+
+// serverError helper writes an error message and stack trace to the errorLog,
+// then sends a generic 500 internal server error to the user.
+func (app *application) serverError(w http.ResponseWriter, err error) {
+	trace := fmt.Sprintf("%s\n%s", err.Error(), debug.Stack())
+	app.errorLog.Output(2, trace)
+
+	http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+}
+
+// clientError helper sends a specific status code and corresponding description
+// to the user.
+func (app *application) clientError(w http.ResponseWriter, status int) {
+	http.Error(w, http.StatusText(status), status)
+}
+
+// notFound helper will return a 404 not found error to the user.
+func (app *application) notFound(w http.ResponseWriter) {
+	app.clientError(w, http.StatusNotFound)
+}
